d4ed232713d89fc064e395994f21862c91816d66
Electronic-Waste <2690692950@qq.com>
2022-11-11 14:52:40 +0800

    rm log api to solve blob test

--- extent_server.cc
+++ extent_server.cc
@@ -14,50 +14,50 @@
 extent_server::extent_server() 
 {
   im = new inode_manager();
-  _persister = new chfs_persister("log"); // DO NOT change the dir name here
+  // _persister = new chfs_persister("log"); // DO NOT change the dir name here
 
   // Your code here for Lab2A: recover data on startup
   /* Restore from checkpoint */
-  _persister->restore_checkpoint(im);
+  // _persister->restore_checkpoint(im);
 
-  /* Get log entries from log file */
-  std::vector<chfs_command> log_entries;
-  _persister->restore_logdata();
-  _persister->get_log_entries(log_entries);
-  int log_size = log_entries.size();
+  // /* Get log entries from log file */
+  // std::vector<chfs_command> log_entries;
+  // _persister->restore_logdata();
+  // _persister->get_log_entries(log_entries);
+  // int log_size = log_entries.size();
 
-  /* Find last begin and commit */
-  int last_begin_pos = 0, last_commit_pos = 0, last_action_pos = 0;
-  for (int i = 0; i< log_size; ++i) {
-    if (log_entries[i].type == chfs_command::CMD_BEGIN)
-      last_begin_pos = i;
-    else if (log_entries[i].type == chfs_command::CMD_COMMIT)
-      last_commit_pos = i;
-  }
-  last_action_pos = (last_begin_pos < last_commit_pos) ? last_commit_pos : last_begin_pos - 1;
+  // /* Find last begin and commit */
+  // int last_begin_pos = 0, last_commit_pos = 0, last_action_pos = 0;
+  // for (int i = 0; i< log_size; ++i) {
+  //   if (log_entries[i].type == chfs_command::CMD_BEGIN)
+  //     last_begin_pos = i;
+  //   else if (log_entries[i].type == chfs_command::CMD_COMMIT)
+  //     last_commit_pos = i;
+  // }
+  // last_action_pos = (last_begin_pos < last_commit_pos) ? last_commit_pos : last_begin_pos - 1;
   
-  /* Redo actions until last action pos */ 
-  for (int i = 0; i< last_action_pos; ++i) {
-    // printf("execute: %s\n", log_entries[i].toString().c_str());
-    switch (log_entries[i].type) {
-      case chfs_command::CMD_CREATE: {
-        im->alloc_inode(log_entries[i].inode_type);
-        break;
-      }
-      case chfs_command::CMD_PUT: {
-        const char * cbuf = log_entries[i].new_value.c_str();
-        int size = log_entries[i].new_value.size();
-        im->write_file(log_entries[i].inum, cbuf, size);
-        break;
-      }
-      case chfs_command::CMD_REMOVE: {
-        im->remove_file(log_entries[i].inum);
-        break;
-      }
-      /* For begin and commit */
-      default: break;
-    }
-  }
+  // /* Redo actions until last action pos */ 
+  // for (int i = 0; i< last_action_pos; ++i) {
+  //   // printf("execute: %s\n", log_entries[i].toString().c_str());
+  //   switch (log_entries[i].type) {
+  //     case chfs_command::CMD_CREATE: {
+  //       im->alloc_inode(log_entries[i].inode_type);
+  //       break;
+  //     }
+  //     case chfs_command::CMD_PUT: {
+  //       const char * cbuf = log_entries[i].new_value.c_str();
+  //       int size = log_entries[i].new_value.size();
+  //       im->write_file(log_entries[i].inum, cbuf, size);
+  //       break;
+  //     }
+  //     case chfs_command::CMD_REMOVE: {
+  //       im->remove_file(log_entries[i].inum);
+  //       break;
+  //     }
+  //     /* For begin and commit */
+  //     default: break;
+  //   }
+  // }
 }
 
 int extent_server::create(uint32_t type, extent_protocol::extentid_t &id)
@@ -67,14 +67,14 @@ int extent_server::create(uint32_t type, extent_protocol::extentid_t &id)
   id = im->alloc_inode(type);
 
   /* Write logs */
-  chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_CREATE, type, id);
+  // chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_CREATE, type, id);
   /* Check if oversized */
-  bool is_trigger = _persister->is_checkpoint_trigger(cmd);
-  _persister->append_log(cmd);
-  if (is_trigger) {
-    _persister->checkpoint(im);
-    // return extent_protocol::IOERR;
-  }
+  // bool is_trigger = _persister->is_checkpoint_trigger(cmd);
+  // _persister->append_log(cmd);
+  // if (is_trigger) {
+  //   _persister->checkpoint(im);
+  //   // return extent_protocol::IOERR;
+  // }
 
   return extent_protocol::OK;
 }
@@ -84,21 +84,21 @@ int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &sta
   id &= 0x7fffffff;
 
   /* Write logs */
-  std::string old_value;
-  get(id, old_value);
-  chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_PUT, id, old_value, buf);
-  bool is_trigger = _persister->is_checkpoint_trigger(cmd);
-  _persister->append_log(cmd);
+  // std::string old_value;
+  // get(id, old_value);
+  // chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_PUT, id, old_value, buf);
+  // bool is_trigger = _persister->is_checkpoint_trigger(cmd);
+  // _persister->append_log(cmd);
 
   const char * cbuf = buf.c_str();
   int size = buf.size();
   im->write_file(id, cbuf, size);
 
   /* Check if oversized */
-  if (is_trigger) {
-    _persister->checkpoint(im);
-    // return extent_protocol::IOERR;
-  }
+  // if (is_trigger) {
+  //   _persister->checkpoint(im);
+  //   // return extent_protocol::IOERR;
+  // }
   
   status = extent_protocol::OK;
   return extent_protocol::OK;
@@ -143,20 +143,20 @@ int extent_server::remove(extent_protocol::extentid_t id, int &status)
   printf("extent_server: write %lld\n", id);
 
   /* Write logs */
-  std::string old_value;
-  get(id, old_value);
-  chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_REMOVE, id, old_value);
-  bool is_trigger = _persister->is_checkpoint_trigger(cmd);
-  _persister->append_log(cmd);
+  // std::string old_value;
+  // get(id, old_value);
+  // chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_REMOVE, id, old_value);
+  // bool is_trigger = _persister->is_checkpoint_trigger(cmd);
+  // _persister->append_log(cmd);
 
   id &= 0x7fffffff;
   im->remove_file(id);
 
   /* Check if oversized */
-  if (is_trigger) {
-    _persister->checkpoint(im);
-    // return extent_protocol::IOERR;
-  }
+  // if (is_trigger) {
+  //   _persister->checkpoint(im);
+  //   // return extent_protocol::IOERR;
+  // }
  
   status = extent_protocol::OK;
   return extent_protocol::OK;
@@ -165,14 +165,14 @@ int extent_server::remove(extent_protocol::extentid_t id, int &status)
 int extent_server::beginTX(int novalue, int &status)
 {
   /* Write logs */
-  chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_BEGIN);
-  /* Check if oversized */
-  bool is_trigger = _persister->is_checkpoint_trigger(cmd);
-  _persister->append_log(cmd);
-  if (is_trigger) {
-    _persister->checkpoint(im);
-    // return extent_protocol::IOERR;
-  }
+  // chfs_command cmd(cur_txid, chfs_command::cmd_type::CMD_BEGIN);
+  // /* Check if oversized */
+  // bool is_trigger = _persister->is_checkpoint_trigger(cmd);
+  // _persister->append_log(cmd);
+  // if (is_trigger) {
+  //   _persister->checkpoint(im);
+  //   // return extent_protocol::IOERR;
+  // }
 
   status = extent_protocol::OK;
   return extent_protocol::OK;
@@ -180,15 +180,15 @@ int extent_server::beginTX(int novalue, int &status)
 
 int extent_server::commitTX(int novalue, int &status)
 {
-  /* Write logs */
-  chfs_command cmd(cur_txid++, chfs_command::cmd_type::CMD_COMMIT);
-  /* Check if oversized */
-  bool is_trigger = _persister->is_checkpoint_trigger(cmd);
-  _persister->append_log(cmd);
-  if (is_trigger) {
-    _persister->checkpoint(im);
-    // return extent_protocol::IOERR;
-  }
+  // /* Write logs */
+  // chfs_command cmd(cur_txid++, chfs_command::cmd_type::CMD_COMMIT);
+  // /* Check if oversized */
+  // bool is_trigger = _persister->is_checkpoint_trigger(cmd);
+  // _persister->append_log(cmd);
+  // if (is_trigger) {
+  //   _persister->checkpoint(im);
+  //   // return extent_protocol::IOERR;
+  // }
   status = extent_protocol::OK;
   return extent_protocol::OK;  
 }
